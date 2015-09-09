17c54725053066e99c95722db68152740404faa9
Case Wright <caseman912@me.com>
2015-09-09 15:51:35 -0400

    Initial Commit

---
+++ MusicBar/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  MusicBar
+//
+//  Created by Case Wright on 9/9/15.
+//  Copyright © 2015 C453. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
